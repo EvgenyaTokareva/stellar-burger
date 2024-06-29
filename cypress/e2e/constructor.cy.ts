5fe6780015ef117daf35561109272be1b58c22e7
Evgeniya Tokareva <multipodarok2012@mail.ru>
2024-06-29 16:01:19 +0500

    вторая версия тестов

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,51 +1,67 @@
 import Cypress from 'cypress';
 const URL_API = 'https://norma.nomoreparties.space/api';
-const ID_BUN = '643d69a5c3f7b9001cfa093e';
-const ID_MAIN = '643d69a5c3f7b9001cfa093d';
+const ID_MAIN = '643d69a5c3f7b9001cfa093e';
+const ID_BUN = '643d69a5c3f7b9001cfa093d';
+const MAIN_SELECTOR = `[data-cy=${ID_MAIN}]`;
+const BUN_SELECTOR = `[data-cy=${ID_BUN}]`;
 
 beforeEach(() => {
+  cy.intercept('GET', `${URL_API}/ingredients`, {
+    fixture: 'ingredients.json'
+  });
   cy.intercept('POST', `${URL_API}/auth/login`, {
     fixture: 'user.json'
   });
   cy.intercept('GET', `${URL_API}/auth/user`, { fixture: 'user.json' });
   cy.intercept('POST', `${URL_API}/orders`, {
     fixture: 'order-burger.json'
-  });  
-  cy.intercept('GET', `${URL_API}/ingredients`, {
-    fixture: 'ingredients.json'
   });
+
+  cy.window().then((window) => {
+    window.localStorage.setItem('refreshToken', 'refresh-test-token');
+  });
+  cy.setCookie('accessToken', 'access-test-token');
+  cy.visit('/');
+
 });
 
-describe('проверка работы ингредиентов', () => {
-  it('проверка работы модального окна', () => {
-    cy.get(`[data-cy=${ID_MAIN}]`).click();
-    cy.get('#modals').find('button').click();
-    cy.get('#modals').should('be.not.empty');
+afterEach(() =>{
+  cy.window().then((window) => {
+    window.localStorage.clear();
   });
-});
+  cy.clearAllCookies();
+})
 
-  it('проверка добавления ингредиента', () => {
-    cy.get(`[data-cy=${ID_MAIN}]`)
+describe('Тестирование работы ингредиентов', () => {
+  it('Добавление ингредиента из списка в конструктор', () => {
+    cy.get(MAIN_SELECTOR)
       .children('button')
       .contains('Добавить')
       .click();
-    cy.get(`[data-cy=${ID_MAIN}]`)
+    cy.get(MAIN_SELECTOR)
       .find('.counter__num')
       .should('contain', '1');
   });
 
-describe('проверка оформления заказа', () => {
-  it('проверка оформления заказа', () => {
-    cy.get(`[data-cy=${ID_MAIN}]`)
+  it('Тестирование работы модального окна', () => {
+    cy.get(MAIN_SELECTOR).click();
+    cy.get('#modals').should('be.not.empty');
+    cy.get('#modals').find('button').click();
+  });
+});
+
+describe('Тестирование оформления заказа', () => {
+  it('Проверка оформления заказа', () => {
+    cy.get(MAIN_SELECTOR)
       .children('button')
       .contains('Добавить')
       .click();
-    cy.get(`[data-cy=${ID_BUN}]`)
+    cy.get(BUN_SELECTOR)
       .children('button')
       .contains('Добавить')
       .click();
     cy.get('button').contains('Оформить заказ').click();
-    cy.get('#modals').find('h2').contains('41304');
+    cy.get('#modals').find('h2').contains('44390');
     cy.get('#modals').find('button').click();
     cy.get('[data-cy=burger-container]').find('li').should('not.exist');
   });
